2f5d515e023259355f9fc9ffd10598532f9eeccc
Joe Post <jpost625@gmail.com>
2026-07-31 14:12:48 -0400

    Lots of misc updates

--- alpha/agents/agent.py
+++ alpha/agents/agent.py
@@ -11,9 +11,13 @@ class AlpacaAgent(ABC):
     
     def __init__(self, agent_name):
         self.order_publication = None
+        self.md_ctrl_publication = None
         self.md_ctrl_subscription = None
         self.md_subscription = None
         self.name = agent_name
+
+    def __del__(self):
+        self.teardown()
 
     def start(self):
         self.order_publication = ArchivePublication(ArchiveConstants.LEDGER_IN_QUEUE)
@@ -27,24 +31,50 @@ class AlpacaAgent(ABC):
         self.md_subscription.subscription_open(ArchiveConstants.DEFAULT_QUEUE_SIZE)
 
     @abstractmethod
-    def execute_strategy(self) -> None:
+    def execute_strategy(self, epoch_sec: float) -> None:
         """Execute trading strategy."""
         pass
 
-    @abstractmethod
     def teardown(self):
         """Teardown this agent, including cancelling any resting orders """
-        pass
+        self.order_publication.publication_close()
+        self.md_ctrl_publication.publication_close()
+        self.md_ctrl_subscription.subscription_close()
+        self.md_subscription.subscription_close()
 
-    def publish_order(self, signal: Optional[OrderRequest]) -> None:
+    def publish_order(self, signal: Optional[OrderRequest]) -> bool:
         if signal is None:
-            return
+            return False
 
-        # TODO: handle other order requests.
-        with super().order_publication.publication_claim(NewOrderSingle) as order:
-            order.orderId = signal.client_order_id
-            order.symbol = signal.symbol
-            order.side = signal.side
-            order.orderQty = signal.quantity
-            order.price = signal.price
-            order.priceFactor = signal.price_factor
+        match signal.action:
+            case OrderAction.NEW:
+                with super().order_publication.publication_claim(NewOrderSingle) as order:
+                    order.orderId = signal.client_order_id
+                    order.symbol = signal.symbol
+                    order.side = signal.side
+                    order.orderQty = signal.quantity
+                    order.price = signal.price
+                    order.priceFactor = signal.price_factor
+                return True
+
+            case OrderAction.CANCEL_REPLACE:
+                with super().order_publication.publication_claim(CancelOrder) as order:
+                    order.orderId = signal.client_order_id
+                    order.symbol = signal.symbol
+                    order.side = signal.side
+                    order.orderQty = signal.quantity
+                    order.price = signal.price
+                    order.priceFactor = signal.price_factor
+                return True
+
+            case OrderAction.CANCEL:
+                with super().order_publication.publication_claim(ReplaceOrder) as order:
+                    order.orderId = signal.client_order_id
+                    order.symbol = signal.symbol
+                    order.side = signal.side
+                    order.orderQty = signal.quantity
+                    order.price = signal.price
+                    order.priceFactor = signal.price_factor
+                return True
+
+        return False
--- alpha/alpaca_client.py
+++ alpha/alpaca_client.py
@@ -2,12 +2,14 @@ import logging
 from typing import List, Optional
 from datetime import datetime
 import json
+import ctypes
 
 from alpaca.data.requests import StockLatestQuoteRequest
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
+from alpha.shared.archive_messages import *
 from alpha.shared.archive_constants import ArchiveConstants
 from alpha.shared.archive_publication import ArchivePublication
 from alpha.shared.archive_subscription import ArchiveSubscription
@@ -34,10 +36,10 @@ class MarketDataClient:
         self.ledger_subscription = None
     
     def start(self):
-        self.md_publication = ArchivePublication(ArchiveConstants.MARKET_DATA_QUEUE)
-        self.ctrl_publication = ArchivePublication(ArchiveConstants.MARKET_DATA_CTRL_RESP)
-        self.ctrl_subscription = ArchiveSubscription(ArchiveConstants.MARKET_DATA_CTRL_RQST)
-        self.ledger_subscription = ArchiveSubscription(ArchiveConstants.LEDGER_OUT_QUEUE)
+        self.md_publication = ArchivePublication(ArchiveConstants.MARKET_DATA_QUEUE, "alpaca_client")
+        self.ctrl_publication = ArchivePublication(ArchiveConstants.MARKET_DATA_CTRL_RESP, "alpaca_client")
+        self.ctrl_subscription = ArchiveSubscription(ArchiveConstants.MARKET_DATA_CTRL_RQST, "alpaca_client")
+        self.ledger_subscription = ArchiveSubscription(ArchiveConstants.LEDGER_OUT_QUEUE, "alpaca_client")
 
     def run(self):
         self._check_and_respond_control_pub()
@@ -47,8 +49,7 @@ class MarketDataClient:
     def _check_and_respond_control_pub(self):
         # TODO: check the control subscription for new symbols to track, symbols to ignore, etc
             # Then respond on the control publication to applicable agents
-        self.ctrl_subscription.subscription_handle
-        pass
+        self.ctrl_subscription.poll_subscription()
 
     def _query_and_publish_market_data(self, symbols: List[str]) -> None:
         # TODO: query market data for tracked symbols and publish any results to market data publication
@@ -132,6 +133,31 @@ class MarketDataClient:
         except Exception as e:
             self.logger.error(f"Failed to publish: {e}")
             return False
+
+    def _on_archive_data(self, bytes, size):
+        print(f"Received {size} bytes from poll: {bytes}")
+        assert(size >= ctypes.sizeof(MessageHeader))
+
+        header = ctypes.cast(bytes, ctypes.POINTER(MessageHeader)).contents
+        match header.templateId:
+            case NewOrderSingle.TEMPLATE_ID:
+                full_message = ctypes.cast(bytes, ctypes.POINTER(NewOrderSingle)).contents
+
+            case CancelOrder.TEMPLATE_ID:
+                full_message = ctypes.cast(bytes, ctypes.POINTER(CancelOrder)).contents
+
+            case ReplaceOrder.TEMPLATE_ID:
+                full_message = ctypes.cast(bytes, ctypes.POINTER(ReplaceOrder)).contents
+
+            case MarketData.TEMPLATE_ID:
+                full_message = ctypes.cast(bytes, ctypes.POINTER(MarketData)).contents
+
+            case MarketDataCtrlRequest.TEMPLATE_ID:
+                full_message = ctypes.cast(bytes, ctypes.POINTER(MarketDataCtrlRequest)).contents
+
+            case MarketDataCtrlResponse.TEMPLATE_ID:
+                full_message = ctypes.cast(bytes, ctypes.POINTER(MarketDataCtrlResponse)).contents
+        
 
 
 if __name__ == "__main__":
